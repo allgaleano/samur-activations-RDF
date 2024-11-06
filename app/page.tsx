6cb4f418944a38cb1d55f5cb52b159ecdf12ac35
allgaleano <all.galeano.diaz@gmail.com>
2024-11-06 15:29:02 +0100

    feat: app completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-"use client";
 import Activations from "@/components/activations";
-import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
--- components/activations.tsx
+++ components/activations.tsx
@@ -1,7 +1,37 @@
-import { useEffect, useState } from "react";
+"use client";
+
+import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
+import Data from "./data";
+
+interface Activation {
+  activationId: string;
+  year: string;
+  month: string;
+  requestTime: string;
+  interventionTime: string;
+  districtLabel: string;
+  hospitalLabel: string;
+  emergencyType: string;
+  districtWikidataLink: string | undefined;
+  hospitalWikidataLink: string | undefined;
+}
+
+interface Binding {
+  label: { value: string };
+  year: { value: string };
+  month: { value: string };
+  requestTime: { value: string };
+  interventionTime: { value: string };
+  districtLabel: { value: string };
+  hospitalLabel: { value: string };
+  emergencyType: { value: string };
+  districtWikidataLink: { value: string };
+  hospitalWikidataLink: { value: string };
+}
 
 const Activations = () => {
-  const [activations, setActivations] = useState([]);
+  const [activations, setActivations] = useState<Activation[]>([]);
 
   async function fetchSPARQL(query: string) {
     const endpoint = 'http://localhost:3030/samur-activations-complete/sparql';
@@ -13,47 +43,47 @@ const Activations = () => {
     });
 
     if (!response.ok) {
-      throw new Error(`SPARQL query failed: ${response.statusText}`);
+      throw new Error('Failed to fetch SPARQL query');
     }
 
     return await response.json();
   }
 
-  async function getActivations() {
+  const getActivations = useCallback(async () => {
     const query = `
-    PREFIX samur: <http://samur.linkeddata.madrid.es/ontology#>
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX schema: <http://schema.org/>
+      PREFIX samur: <http://samur.linkeddata.madrid.es/ontology#>
+      PREFIX owl: <http://www.w3.org/2002/07/owl#>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX schema: <http://schema.org/>
 
-    SELECT ?activation ?year ?month ?requestTime ?interventionTime ?districtLabel ?hospitalLabel ?emergencyType ?districtWikidataLink ?hospitalWikidataLink
-    WHERE {
-      ?activation a samur:Activation ;
-                  samur:hasYear ?year ;
-                  samur:hasMonth ?month ;
-                  samur:hasRequestTime ?requestTime ;
-                  samur:hasInterventionTime ?interventionTime ;
-                  samur:hasEmergencyType ?emergencyType ;
-                  samur:hasDistrict ?district ;
-                  samur:hasHospital ?hospital .
+      SELECT ?activation ?label ?year ?month ?requestTime ?interventionTime ?districtLabel ?hospitalLabel ?emergencyType ?districtWikidataLink ?hospitalWikidataLink
+      WHERE {
+        ?activation a samur:Activation ;
+                    rdfs:label ?label ;  
+                    samur:hasYear ?year ;
+                    samur:hasMonth ?month ;
+                    samur:hasRequestTime ?requestTime ;
+                    samur:hasInterventionTime ?interventionTime ;
+                    samur:hasEmergencyType ?emergencyType ;
+                    samur:hasDistrict ?district ;
+                    samur:hasHospital ?hospital .
 
-      # Get district and hospital labels
-      OPTIONAL { ?district rdfs:label ?districtLabel }
-      OPTIONAL { ?hospital rdfs:label ?hospitalLabel }
+        OPTIONAL { ?district rdfs:label ?districtLabel }
+        OPTIONAL { ?hospital rdfs:label ?hospitalLabel }
 
-      # Get Wikidata links for district and hospital if they exist
-      OPTIONAL { ?district owl:sameAs ?districtWikidataLink }
-      OPTIONAL { ?hospital owl:sameAs ?hospitalWikidataLink }
-    }
-    LIMIT 300
+        OPTIONAL { ?district owl:sameAs ?districtWikidataLink }
+        OPTIONAL { ?hospital owl:sameAs ?hospitalWikidataLink }
+      }
+      ORDER BY DESC(?year) DESC(?month) DESC(?requestTime)
+      LIMIT 300
     `;
 
     const data = await fetchSPARQL(query);
 
-    return data.results.bindings.map((binding: any) => ({
-      activation: binding.activation.value,
+    return data.results.bindings.map((binding: Binding) => ({
+      activationId: binding.label.value,
       year: binding.year.value,
-      month: binding.month.value,
+      month: binding.month.value.replace("--", ""),
       requestTime: binding.requestTime.value,
       interventionTime: binding.interventionTime.value,
       districtLabel: binding.districtLabel ? binding.districtLabel.value : "Unknown",
@@ -62,7 +92,7 @@ const Activations = () => {
       districtWikidataLink: binding.districtWikidataLink ? binding.districtWikidataLink.value : null,
       hospitalWikidataLink: binding.hospitalWikidataLink ? binding.hospitalWikidataLink.value : null,
     }));
-  }
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -70,39 +100,34 @@ const Activations = () => {
       setActivations(activations);
     }
     fetchData();
-  }, []);
+  }, [getActivations]);
 
 
   return (
-    <div className="w-[90%] max-w-[900px] h-full my-10 mx-auto flex flex-col items-center gap-4">
-      <h1 className="font-semibold text-xl">Latest Activations</h1>
+    <div className="w-[90%] max-w-[900px] my-10 mx-auto flex flex-col items-center gap-4">
+      <h1 className="font-semibold text-xl">Latest Samur Activations</h1>
       {activations.length !== 0 ? (
-        <div className="flex flex-col gap-2">
-          {activations.map((activation: any, index) => (
-            <div
-              key={`${activation.activation}-${index}`}
-              className="flex flex-col border w-full rounded-lg py-3 px-4 shadow-sm"
-            >
-              <p><strong>Activation:</strong> {activation.activation}</p>
-              <p><strong>Year:</strong> {activation.year}</p>
-              <p><strong>Month:</strong> {activation.month}</p>
-              <p><strong>Request Time:</strong> {activation.requestTime}</p>
-              <p><strong>Intervention Time:</strong> {activation.interventionTime}</p>
-              <p><strong>District:</strong> {activation.districtLabel} {activation.districtWikidataLink && (
-                <a href={activation.districtWikidataLink} target="_blank" rel="noopener noreferrer">
-                  (View on Wikidata)
-                </a>
-              )}</p>
-              <p><strong>Hospital:</strong> {activation.hospitalLabel} {activation.hospitalWikidataLink && (
-                <a href={activation.hospitalWikidataLink} target="_blank" rel="noopener noreferrer">
-                  (View on Wikidata)
-                </a>
-              )}</p>
-              <p><strong>Emergency Type:</strong> {activation.emergencyType}</p>
+        <div className="flex flex-col gap-2 w-full">
+          {activations.map((activation: Activation) => (
+            <div key={activation.activationId}>
+              <p className="ml-2 mt-2">{activation.activationId}</p>
+              <div
+                className="flex flex-col border w-full rounded-lg py-3 px-4 shadow-sm space-y-2"
+              >
+                <Data label="Year" data={activation.year} />
+                <Data label="Month" data={activation.month} /> 
+                <Data label="Request Time" data={activation.requestTime} />
+                <Data label="Intervention Time" data={activation.interventionTime} />
+                <Data label="Emergency Type" data={activation.emergencyType} />
+                <Data label="District" data={activation.districtLabel} isLink link={activation.districtWikidataLink} />
+                <Data label="Hospital" data={activation.hospitalLabel} isLink link={activation.hospitalWikidataLink} />
+              </div>
             </div>
           ))}
         </div>) : (
-        <p>Loading...</p>
+          <div className="w-[50px] mt-10 h-full flex flex-col items-center justify-center">
+            <Image src="/bars-rotate-fade.svg" alt="Loader" width={50} height={50} className="w-auto h-auto"/>
+          </div>
       )
       }
     </div>
